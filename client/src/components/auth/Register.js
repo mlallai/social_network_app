02dc37907bc230da617b7e338b2139c951ece08c
mlallai <matthieu.lallai@gmail.com>
2018-11-06 18:05:39 +0100

    Create Login Form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -10,16 +10,13 @@ class Register extends Component {
       password2: '',
       errors: {}
     };
-
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  onChange(e) {
-    this.setState({[e.target.name]: e.target.value})
-  }
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+ }
 
-  onSubmit(e) {
+  onSubmit = (e) => {
     e.preventDefault()
 
     const newUser = {
