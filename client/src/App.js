15def63ede81ea6d44b7201d512f890abcd0623f
mlallai <matthieu.lallai@gmail.com>
2018-11-06 16:45:20 +0100

    integration des composants Navbar, Footer et Landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>My React App</h1>
+        <Navbar />
+        {/* <div> */}
+        <Landing />
+        {/* </div> */}
+
+        <Footer />
       </div>
     );
   }
