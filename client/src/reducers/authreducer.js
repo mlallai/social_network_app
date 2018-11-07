8afe2f1760af3c836b63b1f152c2c92dd58f0eec
mlallai <matthieu.lallai@gmail.com>
2018-11-07 23:17:22 +0100

    Reduc Action & ReducerWorkflow Example

--- client/src/reducers/authreducer.js
+++ client/src/reducers/authreducer.js
@@ -1,10 +1,18 @@
+import { TEST_DISPATCH } from '../actions/types';
+
 const initialState = {
     isAuthenticated: false,
     user: {}
 }
 
 export default function (state = initialState, action) {
-    switch(action.type) {
-        default: return state;
+    switch (action.type) {
+        case TEST_DISPATCH:
+        return {
+            ...state,
+            user: action.payload
+        }
+        default:
+            return state;
     }
 }
