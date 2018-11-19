19fcdbc5da88bf1449a1d495d36d998c41b999c8
mlallai <matthieu.lallai@gmail.com>
2018-11-19 14:23:17 +0100

    end of the profile display section

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,8 @@ import EditProfile from './components/edit-profile/EditProfile';
 import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
-
+import Profile from './components/profile/Profile';
+import NotFound from './components/not-found/NotFound';
 
 
 import './App.css';
@@ -65,6 +66,7 @@ class App extends Component {
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/profiles" component={Profiles} />
+        <Route exact path="/profile/:handle" component={Profile} />
         <Switch>
           <PrivateRoute exact path="/dashboard" component={Dashboard} />
         </Switch>
@@ -80,6 +82,7 @@ class App extends Component {
         <Switch>
           <PrivateRoute exact path="/add-education" component={AddEducation} />
         </Switch>
+        <Route exact path="/not-found" component={NotFound} />
         </div>
         <Footer />
       </div>
